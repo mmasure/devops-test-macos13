68a81f6cff30aafe207f74735583476a1f723382
Marc Masure <marc.masure@ext.adeo.com>
2023-05-16 15:14:29 +0200

    added swift hello world code

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Maxime Marinel on 16/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
