6156734f5e38f1df593180842a1e564c3ddd574c
Marc Masure <marc.masure@ext.adeo.com>
2023-05-24 15:33:08 +0200

    added first test

---
+++ HelloWorldTests/HelloWorldTests.swift
@@ -0,0 +1,15 @@
+//
+//  HelloWorldTests.swift
+//  HelloWorldTests
+//
+//  Created by Maxime Marinel on 24/05/2023.
+//
+
+import XCTest
+
+final class HelloWorldTests: XCTestCase {
+
+    func testExample() throws {
+        XCTAssertTrue(true)
+    }
+}
