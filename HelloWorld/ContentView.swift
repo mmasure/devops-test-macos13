d84006fb95aae32d002ad8b61adafcaa305fe374
Marinel Maxime <maxime.marinel@waykonect.com>
2023-05-30 11:38:48 +0200

    Handle avgtool

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -7,6 +7,20 @@
 
 import SwiftUI
 
+extension Bundle {
+    public var appName: String           { getInfo("CFBundleName")  }
+    public var displayName: String       { getInfo("CFBundleDisplayName")}
+    public var language: String          { getInfo("CFBundleDevelopmentRegion")}
+    public var identifier: String        { getInfo("CFBundleIdentifier")}
+    public var copyright: String         { getInfo("NSHumanReadableCopyright").replacingOccurrences(of: "\\\\n", with: "\n") }
+    
+    public var appBuild: String          { getInfo("CFBundleVersion") }
+    public var appVersionLong: String    { getInfo("CFBundleShortVersionString") }
+    //public var appVersionShort: String { getInfo("CFBundleShortVersion") }
+    
+    fileprivate func getInfo(_ str: String) -> String { infoDictionary?[str] as? String ?? "⚠️" }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -14,6 +28,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Text("Ver: \(Bundle.main.appVersionLong) (\(Bundle.main.appBuild)) ")
+        
         }
         .padding()
     }
